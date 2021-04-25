842a8f6f491bc471b9fa7bde211e4e8d34c997da
zelaldenizyildiz <zelaldenizyildiz@gmail.com>
2021-04-26 00:25:07 +0300

    Project

---
+++ Assets/GenerateLevel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateLevel : MonoBehaviour {
+
+	[SerializeField] private Transform levelPart_Start;
+	[SerializeField] private Transform levelPart_1;
+    
+    private void Awake() {
+		Transform lastLevelPartTransform;
+		lastLevelPartTransform = SpawnLevelPart(levelPart_Start.Find("EndPosition").position);
+		lastLevelPartTransform = SpawnLevelPart(lastLevelPartTransform.Find("EndPosition").position);
+		lastLevelPartTransform = SpawnLevelPart(lastLevelPartTransform.Find("EndPosition").position);
+
+    }
+
+	private Transform SpawnLevelPart(Vector3 spawnPosition) {
+		Transform levelPartTransform = Instantiate(levelPart_1, spawnPosition, Quaternion.identity);
+		return levelPartTransform;
+	}
+}
